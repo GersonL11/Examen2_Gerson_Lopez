5fe90faf07ab1a15cdcd7e251482dc818644f42b
GersonL11 <135143582+GersonL11@users.noreply.github.com>
2023-12-16 10:42:02 -0600

    tercer commit

--- lib/helper/answer.dart
+++ lib/helper/answer.dart
@@ -3,13 +3,14 @@ import 'package:examenp3_gersonlopez/domain/entities/respuesta.dart';
 import 'package:http/http.dart' as http;
 
 class ApiAnswer {
-  final String url = "https://stephen-king-api.onrender.com/api/books"; // Nueva URL
+  final String baseUrl = "https://stephen-king-api.onrender.com"; 
 
   Future<List<Respuesta>> getRespuesta() async {
-    final response = await http.get(Uri.parse(url));
+    final response = await http.get(Uri.parse('$baseUrl/api/books'));
 
     if (response.statusCode == 200) {
-      List<dynamic>tdata = json.decode(response.body);
+      
+      final data = Respuesta.fromJson(response.body);
       List<Respuesta> respuestas = data.map((item) => Respuesta.fromJson(item)).toList();
       return respuestas;
     } else {
